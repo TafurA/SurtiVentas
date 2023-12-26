7686f5c2a591938c27a9f7ae1c5baa2cade55f67
Andres Tafur <tafursla@outlook.com>
2023-12-26 10:16:13 -0500

    ---- PRODUCTION --- 5.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ const TOKEN_API_DECODE: string = "SurtiApp:31a5a5b0140e0137e08aadaa60eb016a4b434
 
 export const environment = {
   production: true,
-  API_URL: 'https://surtiappdev.surtilider.com:9001/',
-  // API_URL: 'https://intranet.surtilider.com:9001/',
+  // API_URL: 'https://surtiappdev.surtilider.com:9001/',
+  API_URL: 'https://intranet.surtilider.com:9001/',
   API_PATH: 'IntranetSurti/WebServicesRest',
   headers: {
     'Authorization': `Basic ${encodeTokenApi()}`,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ const TOKEN_API_DECODE: string =  "SurtiApp:31a5a5b0140e0137e08aadaa60eb016a4b43
 
 export const environment = {
   production: false,
-  API_URL: 'https://surtiappdev.surtilider.com:9001/',
-  // API_URL: 'https://intranet.surtilider.com:9001/',
+  // API_URL: 'https://surtiappdev.surtilider.com:9001/',
+  API_URL: 'https://intranet.surtilider.com:9001/',
   API_PATH: 'IntranetSurti/WebServicesRest',
   headers: {
     'Authorization': `Basic ${encodeTokenApi()}`,
